a1b76ee061b651b83618d4e668933606785dbef3
Haval Shaker <Havalshaker@gmail.com>
2026-03-19 13:11:12 +0100

    uppdaterade SQL queries

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -1,1 +1,22 @@
--- delete this later
+-- What are the top paying analyst jobs?
+-- Identify the top 10 highest paying jobs that are available remotely?
+-- Focuses on job postings with specified salaries (remove nulls)
+-- Why? Highlight the top paying opportunities for Data Analystsm offering insight into 
+
+SELECT job_id, job_title, 
+job_location, 
+job_schedule_type, 
+salary_year_avg, 
+job_posted_date,
+name AS company_name
+
+FROM job_postings_fact
+
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+
+WHERE job_title_short = 'Data Analyst' AND
+job_location = 'Anywhere' AND
+salary_year_avg IS NOT NULL
+
+ORDER BY salary_year_avg DESC
+LIMIT 10
